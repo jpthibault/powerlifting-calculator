50e636fc63d8b1232939986a6d6bab4ec5323bf3
Jean-Philip Thibault <jeanphilip.thibault@unity3d.com>
2025-04-12 15:55:05 -0400

    Add Husky pre-commit hook and lint-staged configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./routes/App";
-import Settings from "./routes/Settings";
 import "./index.css";
 import { SettingsProvider } from "./SettingsContext";
 
