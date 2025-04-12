d8b86163c2605a2e7d2ce1845b38d95554f1b974
Jean-Philip Thibault <jeanphilip.thibault@unity3d.com>
2025-04-12 12:31:33 -0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/powerlift/", // Set the base path for GitHub Pages
+  base: "/powerlifting-calculator/", // Set the base path for GitHub Pages
   build: {
     outDir: "dist",
   },
