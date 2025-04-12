f6162298d7984a81344a32a611915495c7d8c481
Jean-Philip Thibault <jeanphilip.thibault@unity3d.com>
2025-04-12 13:47:55 -0400

    Configure deployment to GitHub Pages with GitHub Actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/powerlifting-calculator/", // Set the base path for GitHub Pages
-  build: {
-    outDir: "dist",
-  },
 });
