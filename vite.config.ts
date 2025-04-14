c126806ca7dc6a4677100e52bd30607f164b379d
Jean-Philip Thibault <jeanphilip.thibault@unity3d.com>
2025-04-14 08:09:57 -0400

    Merged tsconfig.node.json into tsconfig.json and removed unnecessary file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
-    react(),
-  ],
+  plugins: [react()],
   base: "/powerlifting-calculator/", // Set the base path for GitHub Pages
 });
