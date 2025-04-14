f225a618911fd8ed395c5ee806ef94df6858e24d
Jean-Philip Thibault <jeanphilip.thibault@unity3d.com>
2025-04-14 08:22:42 -0400

    Fix CSS module imports by adding type declarations and updating tsconfig

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+
+// CSS modules
+declare module '*.css' {
+  const css: { [key: string]: string };
+  export default css;
+}
+
+// Image formats
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg' {
+  import React from 'react';
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
